60a30b502f225a5e92c07d1b50f7f5d9ee3600d2
Undersent <rafallawro@gmail.com>
2017-11-03 12:57:01 +0100

    added specs to controller

--- src/main/java/com/example/demo/persistence/dao/UserSpecificationBuilder.java
+++ src/main/java/com/example/demo/persistence/dao/UserSpecificationBuilder.java
@@ -1,4 +1,40 @@
 package com.example.demo.persistence.dao;
 
+import com.example.demo.persistence.model.User;
+import com.example.demo.web.util.SearchCriteria;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserSpecificationBuilder {
+
+    private final List<SearchCriteria> params;
+
+    public UserSpecificationBuilder() {
+        this.params = new ArrayList<SearchCriteria>();
+    }
+
+    public UserSpecificationBuilder with(String key, String operation, Object value) {
+        params.add(new SearchCriteria(key, operation, value));
+        return this;
+    }
+
+    public Specification<User> build() {
+        if (params.size() == 0) {
+            return null;
+        }
+
+        List<Specification<User>> specs = new ArrayList<Specification<User>>();
+        for (SearchCriteria param : params) {
+            specs.add(new UserSpecification(param));
+        }
+
+        Specification<User> result = specs.get(0);
+        for (int i = 1; i < specs.size(); i++) {
+            result = Specifications.where(result).and(specs.get(i));
+        }
+        return result;
+    }
 }
--- src/main/java/com/example/demo/web/controller/UserController.java
+++ src/main/java/com/example/demo/web/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.example.demo.web.controller;
 
 import com.example.demo.persistence.dao.IUserDao;
+import com.example.demo.persistence.dao.UserSpecificationBuilder;
+import com.example.demo.persistence.dao.repository.UserRepository;
 import com.example.demo.persistence.model.User;
 import com.example.demo.web.util.SearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +20,14 @@ import java.util.regex.Pattern;
 public class UserController {
 
     private final IUserDao api;
-
+    private final UserRepository repo;
     @Autowired
-    public UserController(IUserDao api) {
+    public UserController(IUserDao api, UserRepository repo) {
         this.api = api;
+        this.repo = repo;
     }
 
-    @GetMapping("/users") //http://localhost:8080/users?search=lastName:Bozenka,age>25
+    @GetMapping("/users") //users?search=lastName:Bozenka,age>25
     public List<User> findAll(@RequestParam(value = "search", required = false)String search){
         List<SearchCriteria> params = new ArrayList<SearchCriteria>();
         if(search != null) {
@@ -35,5 +39,18 @@ public class UserController {
             }
         }
         return api.searchUser(params);
+    }
+
+    @GetMapping("/users/specs") //users/specs?search=lastName:Bozenka,age>25
+    public List<User> search(@RequestParam(value = "search") String search) {
+        UserSpecificationBuilder builder = new UserSpecificationBuilder();
+        Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+        Matcher matcher = pattern.matcher(search + ",");
+        while (matcher.find()) {
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+        }
+
+        Specification<User> spec = builder.build();
+        return repo.findAll(spec);
     }
 }
